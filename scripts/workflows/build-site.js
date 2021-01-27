92afa163bbe97ca6e246a886c2a947be2782842b
Alex Beaman <alex@insidenewcity.com>
2021-01-27 12:48:39 -0700

    try getting action input

--- scripts/workflows/build-site.js
+++ scripts/workflows/build-site.js
@@ -1,13 +1,44 @@
 const { Octokit } = require("@octokit/core");
+const core = require("@actions/core");
+
+try {
+    // `who-to-greet` input defined in action metadata file
+    const siteData = core.getInput("site");
+    console.log("site data:");
+    console.log(siteData);
+    // const time = (new Date()).toTimeString();
+    // core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
+} catch (error) {
+    core.setFailed(error.message);
+}
+console.log("done");
+return;
+
+const OCTOKIT_TOKEN = process.env.OCTOKIT_TOKEN || null;
+
+if (!OCTOKIT_TOKEN) {
+    console.log("Err: Missing Octokit Token.");
+    return;
+}
 
 console.log("init Octokit");
 
 // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
 const octokit = new Octokit({
-    auth: process.env.OCTOKIT_TOKEN || "",
+    auth: OCTOKIT_TOKEN,
 });
 
-console.log("get file");
+const FILES = {
+    PRIMO_JSON: {
+        path: "",
+    },
+    FILE_HTML,
+    FILE_JS,
+    FILE_CSS,
+};
 
 // https://docs.github.com/en/rest/reference/actions#create-a-workflow-dispatch-event
 octokit
